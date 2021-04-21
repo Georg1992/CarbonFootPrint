f1722ba3398d62d18a524602f58f2fb95f59157f
rikotee <Teemu.Rekol2@metropolia.fi>
2021-04-21 14:48:30 +0300

    small fix

--- CarbonFootprintTracking/PieViewController.swift
+++ CarbonFootprintTracking/PieViewController.swift
@@ -8,44 +8,58 @@
 import Charts
 import UIKit
 
-class PieViewController: UIViewController, ChartViewDelegate{
+class PieViewController: UIViewController{
     
-    @IBOutlet var pieChartView: PieChartView!
-    
-    var pieChart = PieChartView()
+    @IBOutlet weak var pieChartView: PieChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pieChart.delegate = self
         
+        let transport = ["train", "metro", "car", "plane", "boat", "bike"]
+        let carbon = [6, 8, 26, 30, 8, 10]
+        
+        setChart(dataPoints: transport, values: carbon.map{ Double($0) })
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+    func setChart(dataPoints: [String], values: [Double]) {
         
-        pieChart.frame = CGRect(x: 0, y: 0,
-                                width: self.view.frame.size.width,
-                                height: self.view.frame.size.width)
-        pieChart.center = view.center
-        view.addSubview(pieChart)
+        // 1. Set ChartDataEntry
+        var dataEntries: [ChartDataEntry] = []
+        for i in 0..<dataPoints.count {
+            let dataEntry = PieChartDataEntry(value: values[i], label: dataPoints[i], data: dataPoints[i] as AnyObject)
+            dataEntries.append(dataEntry)
+        }
         
+        // 2. Set ChartDataSet
+        let set = PieChartDataSet(entries: dataEntries)
+        set.colors = colorsOfCharts(numbersOfColor: dataPoints.count)
+        //set.colors = ChartColorTemplates.pastel()
+        pieChartView.frame = CGRect(x: 0, y: 0,
+                                    width: self.view.frame.size.width,
+                                    height: self.view.frame.size.width)
+        //pieChartView.center = view.center
         
-        //data
-        let set = PieChartDataSet(entries: [
-            
-            PieChartDataEntry(value: 1,
-                              data: 1),
-            
-            PieChartDataEntry(value: 5,
-                              data: 5),
-            
-            PieChartDataEntry(value: 9,
-                              data: 8),
-            
-        ])
-        
-        set.colors = ChartColorTemplates.pastel()
+        // 3. Set ChartData
         let data = PieChartData(dataSet: set)
-        pieChart.data = data
+        let format = NumberFormatter()
+        format.numberStyle = .none
+        let formatter = DefaultValueFormatter(formatter: format)
+        data.setValueFormatter(formatter)
+        
+        // 4. Assign it to the chart’s data
+        pieChartView.data = data
+    }
+    
+    private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
+        var colors: [UIColor] = []
+        for _ in 0..<numbersOfColor {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+            pieChartView.backgroundColor = UIColor.white
+        }
+        return colors
     }
 }
