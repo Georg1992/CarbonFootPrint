919c469a73e7ce9208b596b72756379e4fc53426
rikotee <Teemu.Rekol2@metropolia.fi>
2021-04-22 13:06:27 +0300

    fix

--- CarbonFootprintTracking/DataSetStatistics.swift
+++ CarbonFootprintTracking/DataSetStatistics.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct DataSetStatistics {
+    private(set) var timeStamp: String?
+    private(set) var carbonFootprint: Int?
+    private(set) var vehicleType: String?
+    
+    init(timeStamp: String, carbonFootprint: Int, vehicleType: String) {
+        self.timeStamp = timeStamp
+        self.carbonFootprint = carbonFootprint
+        self.vehicleType = vehicleType
+    }
+}
