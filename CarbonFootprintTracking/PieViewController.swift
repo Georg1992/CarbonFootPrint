0101d6b39ff2d1a0eeadfb5071228c94c5998fed
rikotee <Teemu.Rekol2@metropolia.fi>
2021-04-26 14:58:11 +0300

    added monthly budget progress bar

--- CarbonFootprintTracking/PieViewController.swift
+++ CarbonFootprintTracking/PieViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PieViewController: UIViewController{
     
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var pieChartView: PieChartView!
     
     override func viewDidLoad() {
@@ -19,6 +21,8 @@ class PieViewController: UIViewController{
         let carbon = [6, 8, 26, 30, 8, 10]
         
         setChart(dataPoints: transport, values: carbon.map{ Double($0) })
+        
+        budgetBar()
     }
     
     func setChart(dataPoints: [String], values: [Double]) {
@@ -61,5 +65,79 @@ class PieViewController: UIViewController{
             pieChartView.backgroundColor = UIColor.white
         }
         return colors
+    }
+    
+    func budgetBar() {
+        // Start value
+        progressView.progress = Float(budgetProgress())
+        
+        // Style
+        progressView.progressTintColor = UIColor.green
+        //progressView.backgroundColor = UIColor.systemBackground
+        progressView.layer.cornerRadius = 5
+        progressView.clipsToBounds = true
+        progressView.layer.sublayers![1].cornerRadius = 5
+        progressView.subviews[1].clipsToBounds = true
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 8)
+        updateProgressView()
+    }
+    
+    func budgetProgress() -> Double {
+        let prog: Double = 0
+        //test value
+        let addTestCar: Double = 0.3
+        let allProg = prog+addTestCar
+        return allProg
+    }
+    
+    @objc func updateProgressView(){
+        progressView.progress += 0.1
+        progressView.setProgress(progressView.progress, animated: true)
+        
+        if(progressView.progress >= 0.0) {
+            textLabel.text = "used 0%"
+            progressView.progressTintColor = UIColor.green
+        }
+        if(progressView.progress >= 0.1) {
+            textLabel.text = "used 10%"
+            progressView.progressTintColor = UIColor.green
+        }
+        if(progressView.progress >= 0.2) {
+            textLabel.text = "used 20%"
+            progressView.progressTintColor = UIColor.green
+        }
+        if(progressView.progress >= 0.3) {
+            textLabel.text = "used 30%"
+            progressView.progressTintColor = UIColor.green
+        }
+        if(progressView.progress >= 0.4) {
+            textLabel.text = "used 40%"
+            progressView.progressTintColor = UIColor.green
+        }
+        if(progressView.progress >= 0.5) {
+            textLabel.text = "used 50%"
+            progressView.progressTintColor = UIColor.green
+        }
+        if(progressView.progress >= 0.6) {
+            textLabel.text = "used 60%"
+            progressView.progressTintColor = UIColor.green
+        }
+        if(progressView.progress >= 0.7) {
+            textLabel.text = "used 70%"
+            progressView.progressTintColor = UIColor.orange
+        }
+        if(progressView.progress >= 0.8) {
+            textLabel.text = "used 80%"
+            progressView.progressTintColor = UIColor.orange
+        }
+        if(progressView.progress >= 0.9) {
+            textLabel.text = "used 90%"
+            progressView.progressTintColor = UIColor.orange
+        }
+        if (progressView.progress == 1.0) {
+            textLabel.text = "used 100%"
+            print("full")
+            progressView.progressTintColor = UIColor.red
+        }
     }
 }
