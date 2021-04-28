50bd5d3ad5f5a3fe5b17f92db4e34037e183cf98
oskarwi <oskar.wiiala@metropolia.fi>
2021-04-28 22:28:17 +0300

    more linechart stuff fixed

--- CarbonFootprintTracking/StatisticsViewController.swift
+++ CarbonFootprintTracking/StatisticsViewController.swift
@@ -20,9 +20,13 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
     
     @IBOutlet weak var yearButtonOutlet: UIButton!
     
+    @IBOutlet weak var containerChart: UIView!
+    
    @IBOutlet weak var vehicleTextField: UITextField!
+    
     var selectedVehicle: String?
-    var vehicleTypes = ["Car", "Plane", "Train", "Bike", "Scooter", "Metro"]
+    var vehicleTypes = ["All", "Car", "Plane", "Train", "Bike", "Scooter", "Metro"]
+    var currentVehicle = "All"
     
     lazy var lineChartView: LineChartView = {
         let chartView = LineChartView()
@@ -43,7 +47,7 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         xAxis.labelTextColor = .gray
         xAxis.axisLineColor = .gray
         xAxis.labelFont = .boldSystemFont(ofSize: 20)
-        xAxis.setLabelCount(7, force: false)
+        xAxis.setLabelCount(12, force: true)
         
         chartView.animate(xAxisDuration: 1.0)
         chartView.legend.font = .boldSystemFont(ofSize: 20)
@@ -51,9 +55,6 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         return chartView
     }()
     
-    //set of vehicle types
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -62,14 +63,14 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         self.createAndSetupPickerView()
         self.dismissAndClosePickerView()
         
+        //making lineChartView a subView of containerChart for easy constraints
+        containerChart.addSubview(lineChartView)
         
-        // adding the linechartview to our view
-        view.addSubview(lineChartView)
-        lineChartView.centerInSuperview()
         
-        //setting width and height of linechartview according to the width of the screen
-        lineChartView.width(to: view)
-        lineChartView.heightToWidth(of: view)
+        //setting width and height of linechartview according to the width of the superview
+        lineChartView.width(to: containerChart)
+        lineChartView.heightToWidth(of: containerChart)
+        lineChartView.topToSuperview()
         
         let carTripData = DataSetStatistics(timeStamp: "12345", carbonFootprint: 10.0, vehicleType: "Car")
         
@@ -90,6 +91,7 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         self.vehicleTextField.inputView = pickerview
     }
     
+    //Closes and dismisses pickerView
     func dismissAndClosePickerView(){
         
         let toolBar = UIToolbar()
@@ -104,13 +106,14 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         self.view.endEditing(true)
     }
 
+    //when user clicks on statistics chart, user can see the exact value
     func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
         print(entry)
     }
     
     //sets data for the linechartview
     func setData() {
-        let set1 = LineChartDataSet(entries: yValues, label: "CO2 footprint")
+        let set1 = LineChartDataSet(entries: dayValues, label: "CO2 footprint")
         set1.mode = .cubicBezier
         set1.drawCirclesEnabled = false
         set1.lineWidth = 3
@@ -120,7 +123,6 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         set1.drawFilledEnabled = true
         
         
-        //set1.drawHorizontalHighlightIndicatorEnabled = false
         set1.highlightColor = .black
         set1.highlightLineWidth = 1.5
         
@@ -131,7 +133,7 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
     }
     
     //Dummy data for chart
-    let yValues: [ChartDataEntry] = [
+    let dayValues: [ChartDataEntry] = [
         ChartDataEntry(x: 0.0, y: 10.0),
         ChartDataEntry(x: 1.0, y: 15.0),
         ChartDataEntry(x: 2.0, y: 17.0),
@@ -144,9 +146,21 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         ChartDataEntry(x: 9.0, y: 11.0),
         ChartDataEntry(x: 10.0, y: 24.0),
         ChartDataEntry(x: 11.0, y: 3.0),
+        ChartDataEntry(x: 12.0, y: 10.0),
+        ChartDataEntry(x: 13.0, y: 15.0),
+        ChartDataEntry(x: 14.0, y: 17.0),
+        ChartDataEntry(x: 15.0, y: 6.0),
+        ChartDataEntry(x: 16.0, y: 2.0),
+        ChartDataEntry(x: 17.0, y: 20.0),
+        ChartDataEntry(x: 18.0, y: 7.0),
+        ChartDataEntry(x: 19.0, y: 10.0),
+        ChartDataEntry(x: 20.0, y: 10.0),
+        ChartDataEntry(x: 21.0, y: 11.0),
+        ChartDataEntry(x: 22.0, y: 24.0),
+        ChartDataEntry(x: 23.0, y: 3.0),
     ]
     
-    let yValues2: [ChartDataEntry] = [
+    let weekValues: [ChartDataEntry] = [
         ChartDataEntry(x: 0.0, y: 5.0),
         ChartDataEntry(x: 1.0, y: 35.0),
         ChartDataEntry(x: 2.0, y: 45.0),
@@ -156,6 +170,56 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         ChartDataEntry(x: 6.0, y: 20.0),
     ]
     
+    let monthValues: [ChartDataEntry] = [
+        ChartDataEntry(x: 0.0, y: 10.0),
+        ChartDataEntry(x: 1.0, y: 15.0),
+        ChartDataEntry(x: 2.0, y: 17.0),
+        ChartDataEntry(x: 3.0, y: 6.0),
+        ChartDataEntry(x: 4.0, y: 2.0),
+        ChartDataEntry(x: 5.0, y: 20.0),
+        ChartDataEntry(x: 6.0, y: 7.0),
+        ChartDataEntry(x: 7.0, y: 10.0),
+        ChartDataEntry(x: 8.0, y: 10.0),
+        ChartDataEntry(x: 9.0, y: 11.0),
+        ChartDataEntry(x: 10.0, y: 24.0),
+        ChartDataEntry(x: 11.0, y: 3.0),
+        ChartDataEntry(x: 12.0, y: 10.0),
+        ChartDataEntry(x: 13.0, y: 15.0),
+        ChartDataEntry(x: 14.0, y: 17.0),
+        ChartDataEntry(x: 15.0, y: 6.0),
+        ChartDataEntry(x: 16.0, y: 2.0),
+        ChartDataEntry(x: 17.0, y: 20.0),
+        ChartDataEntry(x: 18.0, y: 7.0),
+        ChartDataEntry(x: 19.0, y: 10.0),
+        ChartDataEntry(x: 20.0, y: 10.0),
+        ChartDataEntry(x: 21.0, y: 11.0),
+        ChartDataEntry(x: 22.0, y: 24.0),
+        ChartDataEntry(x: 23.0, y: 3.0),
+        ChartDataEntry(x: 24.0, y: 20.0),
+        ChartDataEntry(x: 25.0, y: 7.0),
+        ChartDataEntry(x: 26.0, y: 10.0),
+        ChartDataEntry(x: 27.0, y: 10.0),
+        ChartDataEntry(x: 28.0, y: 11.0),
+        ChartDataEntry(x: 29.0, y: 24.0),
+        ChartDataEntry(x: 30.0, y: 3.0),
+        ChartDataEntry(x: 31.0, y: 3.0),
+    ]
+    
+    let yearValues: [ChartDataEntry] = [
+        ChartDataEntry(x: 0.0, y: 5.0),
+        ChartDataEntry(x: 1.0, y: 35.0),
+        ChartDataEntry(x: 2.0, y: 45.0),
+        ChartDataEntry(x: 3.0, y: 35.0),
+        ChartDataEntry(x: 4.0, y: 20.0),
+        ChartDataEntry(x: 5.0, y: 20.0),
+        ChartDataEntry(x: 6.0, y: 20.0),
+        ChartDataEntry(x: 7.0, y: 45.0),
+        ChartDataEntry(x: 8.0, y: 35.0),
+        ChartDataEntry(x: 9.0, y: 20.0),
+        ChartDataEntry(x: 10.0, y: 20.0),
+        ChartDataEntry(x: 11.0, y: 20.0),
+    ]
+    
     @IBAction func dayButtonPressed(_ sender: UIButton) {
         print("dayButtonPressed")
         setData()
@@ -163,7 +227,9 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
     
     @IBAction func weekButtonPressed(_ sender: UIButton) {
         print("weekButtonPressed")
-        let set2 = LineChartDataSet(entries: yValues2, label: "CO2 footprint")
+        
+        //sets values for the data set and some front end stuff for the red line in the chart
+        let set2 = LineChartDataSet(entries: weekValues, label: "CO2 footprint")
         set2.mode = .cubicBezier
         set2.drawCirclesEnabled = false
         set2.lineWidth = 3
@@ -172,8 +238,7 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
         set2.fillAlpha = 0.65
         set2.drawFilledEnabled = true
         
-        
-        //set1.drawHorizontalHighlightIndicatorEnabled = false
+        //sets values for the highlight lines when clicking on the chart
         set2.highlightColor = .black
         set2.highlightLineWidth = 1.5
         
@@ -202,6 +267,7 @@ class StatisticsViewController: UIViewController, ChartViewDelegate {
 
 }
 
+//An extension to make a pickerView to select vehicles
 extension StatisticsViewController: UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -219,5 +285,8 @@ extension StatisticsViewController: UIPickerViewDelegate, UIPickerViewDataSource
         self.selectedVehicle = self.vehicleTypes[row]
         print(self.selectedVehicle ?? "none")
         self.vehicleTextField.text = self.selectedVehicle
+        
+        //sets the current vehicle used
+        self.currentVehicle = self.selectedVehicle ?? "All"
     }
 }
