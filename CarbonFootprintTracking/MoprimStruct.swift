aaee99f51d2c27c1c6deb7ff2d323e746d0b9d90
rikotee <Teemu.Rekol2@metropolia.fi>
2021-05-05 20:38:56 +0300

    added update button to pie

--- CarbonFootprintTracking/MoprimStruct.swift
+++ CarbonFootprintTracking/MoprimStruct.swift
@@ -23,21 +23,6 @@ struct Properties: Codable {
         case co2 = "CO_2"
         case activity
     }
-    
-    /*
-    init(from decoder: Decoder) throws {
-      let container = try decoder.container(keyedBy: CodingKeys.self)
-      self.co2 = try container.decode(Double.self, forKey: .co2)
-      self.activity = try container.decode(String.self, forKey: .activity)
-    }
-     */
-     /*
-    func encode(to encoder: Encoder) throws {
-      var container = encoder.container(keyedBy: CodingKeys.self)
-      try container.encode(self.co2, forKey: .co2)
-      try container.encode(self.activity, forKey: .activity)
-    }
- */
 }
 
 
