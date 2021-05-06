044a78c55b2539eccc881a8ab8f8cdbfe94c123c
rikotee <Teemu.Rekol2@metropolia.fi>
2021-05-06 14:56:25 +0300

    added custom labels

--- CarbonFootprintTracking/Styles.swift
+++ CarbonFootprintTracking/Styles.swift
@@ -8,23 +8,6 @@
 import Foundation
 import UIKit
 
-extension UIButton {
-    func applyDesing() {
-        // button style
-        self.backgroundColor = UIColor.orange
-        self.layer.cornerRadius = 7
-        self.setTitleColor(UIColor.white, for: .normal)
-        
-        /*
-        //shadow
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowRadius = 2
-        self.layer.shadowOpacity = 0.5
-        self.layer.shadowOffset = CGSize(width: 0, height: 0)
-        */
-    }
-}
-
 class roundButton: UIButton {
     override func didMoveToWindow() {
         // button style
@@ -41,3 +24,12 @@ class roundButton: UIButton {
         */
     }
 }
+
+class customLabel: UILabel {
+    override func didMoveToWindow() {
+        self.textColor = UIColor.gray
+    }
+}
+
+// to change backroundcolor: in viewDidLoad()
+//  self.view.backgroundColor = UIColor.orange
